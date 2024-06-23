ae5e37d1acec5c0a53082730fa5b7800f9f8b9c9
Maira <makyp002@gmail.com>
2024-06-23 18:14:58 -0500

    Se hizo funcional la funcion de añadir una tarea y eliminar una tarea, de este mismo modo se creo un modelo para crear nuevas tareas y de esta manera un objeto cumpla con los requerimientos establecidos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Task } from './../../models/task.models'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      tittle: 'Estudiar Angular',
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      tittle: 'Crear proyecto de Angular',
+      completed: false,
+    },
+    {
+      id: Date.now(),
+      tittle: 'Hacer ejercicio',
+      completed: false,
+    }
+  ]);
 
+  changeHandler(event:Event){
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.addTask(newTask);
+  }
+
+  addTask(tittle: string){
+    const newTask = {
+      id : Date.now(),
+      tittle,
+      completed: false,
+    };
+    //Como agregar una nueva tarea
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+
+  }
 }
